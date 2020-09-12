e56aab8ccdd5f164a624d80f83571cad05878a77
jory8 <44974734+jory8@users.noreply.github.com>
2020-09-12 22:08:17 +0200

    récupération utilisateur existant OK

--- src/main/java/com/joolbite/incognito/controller/AuthentificationController.java
+++ src/main/java/com/joolbite/incognito/controller/AuthentificationController.java
@@ -4,20 +4,22 @@ import com.joolbite.incognito.model.Utilisateur;
 import com.joolbite.incognito.model.UtilisateurAuthData;
 import com.joolbite.incognito.service.authentification.AuthentificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin("*")
+@CrossOrigin
 public class AuthentificationController {
 
     @Autowired
     private AuthentificationService authService;
 
-    @GetMapping("")
+    @PostMapping(value = "/authentification", consumes = MediaType.APPLICATION_JSON_VALUE)
     public UtilisateurAuthData findJory(@RequestBody Utilisateur utilisateur
     ) {
+        System.out.println("ça passe par le back");
         UtilisateurAuthData utilisateurExistant = authService.rechercherUtilisateur(utilisateur);
         return utilisateurExistant;
     }
